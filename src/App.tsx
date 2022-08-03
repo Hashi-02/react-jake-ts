7225cea930029c3427a916a4a9cbe924f408e22b
hashi-02 <hassie0209@gmail.com>
2022-08-04 01:14:44 +0900

    jsonplaceholderを使ったデータの取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import axios from 'axios';
 import './App.css';
 import { UserCard } from './components11/UserCard';
-
-const user = {
-  id: 1,
-  name: 'はし',
-  email: 'example@nnn.jp',
-  address: 'Japan',
-};
+import { User } from './types/api/user';
+import { useState } from 'react';
+import { UserProfile } from './types/UserProfile';
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
